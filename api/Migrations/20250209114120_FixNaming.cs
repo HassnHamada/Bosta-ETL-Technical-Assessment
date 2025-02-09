9a535dc10fb5da3e3a586839862c715ad96d9f77
Hassn Hamada <hassneltokhy4@gmail.com>
2025-02-09 13:42:16 +0200

    DB migrate

---
+++ api/Migrations/20250209114120_FixNaming.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixNaming : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "longitude",
+                table: "Address",
+                newName: "Longitude");
+
+            migrationBuilder.RenameColumn(
+                name: "latitude",
+                table: "Address",
+                newName: "Latitude");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Longitude",
+                table: "Address",
+                newName: "longitude");
+
+            migrationBuilder.RenameColumn(
+                name: "Latitude",
+                table: "Address",
+                newName: "latitude");
+        }
+    }
+}
