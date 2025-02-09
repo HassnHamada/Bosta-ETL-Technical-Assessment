dacfd769d4575eebddd4787dcb9aa424600f7601
Hassn Hamada <hassneltokhy4@gmail.com>
2025-02-09 18:50:19 +0200

    Convert to csv logic

---
+++ convert.py
@@ -0,0 +1,10 @@
+import csv
+
+
+def save_to_csv(data: list[dict], filename: str):
+    with open(filename, 'w', newline='') as csvfile:
+        fieldnames = data[0].keys()
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for row in data:
+            writer.writerow(row)
